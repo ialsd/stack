3634ef30b461b4825bc3459ba49b52064c979695
ialsd <103018975+ialsd@users.noreply.github.com>
2022-04-29 19:31:18 +0500

    Add files via upload

---
+++ ListStack.cpp
@@ -0,0 +1,42 @@
+#include "ListStack.h"
+
+ListStack::ListStack()
+{
+    _list = new list<ValueType>;
+}
+
+ListStack::ListStack(const ValueType* rawArray, const size_t size)
+{
+    _list = new list<ValueType>;
+    _list->insert(_list->begin(), &rawArray[0], &rawArray[size]);
+}
+// добавление в хвост
+void ListStack::push(const ValueType& value)
+{
+    _list->push_back(value);
+}
+// удаление с хвоста
+void ListStack::pop()
+{
+    _list->pop_back();
+}
+// посмотреть элемент в хвосте
+const ValueType& ListStack::top() const 
+{
+    return _list->back();
+}
+// проверка на пустоту
+bool ListStack::isEmpty() const
+{
+    return _list->empty();
+}
+// размер 
+size_t ListStack::size() const
+{
+    return _list->size();
+}
+// виртуальный деструктор
+ListStack::~ListStack()
+{
+    delete _list;
+}
---
+++ VectorStack.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "StackImplementation.h"
+#include <vector>
+using namespace std;
+
+class VectorStack : public IStackImplementation {
+public:
+    VectorStack();
+    VectorStack(const ValueType* rawArray, const size_t size);
+    // добавление в хвост
+    void push(const ValueType& value) override;
+    // удаление с хвоста
+    void pop() override;
+    // посмотреть элемент в хвосте
+    const ValueType& top() const override;
+    // проверка на пустоту
+    bool isEmpty() const override;
+    // размер 
+    size_t size() const override;
+    // виртуальный деструктор
+    ~VectorStack() override;
+private:
+    vector<ValueType>* _vector;
+};
