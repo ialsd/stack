83ed1ae9aff29517be25e2ef6b43552856f0f2a0
ialsd <103018975+ialsd@users.noreply.github.com>
2022-06-06 23:15:37 +0500

    Update VectorStack.h

--- VectorStack.h
+++ VectorStack.h
@@ -3,19 +3,15 @@
 #include <vector>
 using namespace std;
 
-class VectorStack : public IStackImplementation {
+class VectorStack : public IStackImplementation 
+{
 public:
     VectorStack() = default;
     VectorStack(const ValueType* rawArray, const size_t size);
-    // добавление в хвост
     void push(const ValueType& value) override;
-    // удаление с хвоста
     void pop() override;
-    // посмотреть элемент в хвосте
     const ValueType& top() const override;
-    // проверка на пустоту
     bool isEmpty() const override;
-    // размер 
     size_t size() const override;
 private:
     vector<ValueType> _vector;
